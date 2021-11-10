b2cbc440ae11dfe40b496609273683305a260f20
PotapovE <potapovomsk@gmail.com>
2021-11-10 23:18:54 +0300

    решение задачи 15

--- Practice/Program.cs
+++ Practice/Program.cs
@@ -137,11 +137,26 @@
 // Console.Write($"{valueArr}");
 
 // 14. Найти третью цифру числа или сообщить, что её нет
-int numbers (int a) {
-    if (a < 0) {a = a*-1;}
-    if (a > 99) {return a = a % 1000 / 100;}
-    else {return a = -1;}
-}
-int randomNumb = new Random().Next(-100, 100);
-int valueArr = numbers(randomNumb);
-Console.Write($"Третья цифра числа {randomNumb}:\n {valueArr}");
+// int numbers (int a) {
+//     if (a < 0) {a = a*-1;}
+//     if (a > 99) {return a = a % 1000 / 100;}
+//     else {return a = -1;}
+// }
+// int randomNumb = new Random().Next(-100, 100);
+// int valueArr = numbers(randomNumb);
+// Console.Write($"Третья цифра числа {randomNumb}:\n {valueArr}");
+
+// Почувствуй себя джуном
+// 15. Дано число. Проверить кратно ли оно 7 и 23
+bool valBool (int a) {return a % 7 == 0 & a % 23 == 0;}
+int randomNumb = new Random().Next(161, 165);
+bool res = valBool(randomNumb); // 161 сработает как true
+Console.Write($"Cлучайное число {randomNumb}: {res}");
+
+// 16. Дано число обозначающее день недели. Выяснить является номер недели выходным днём
+// 17. По двум введённым числам проверять является ли одно квадратом другого
+// 18. Проверить истинность утверждения ¬(X ⋁ Y) = ¬X ⋀ ¬Y
+// 19. Определить номер четверти плоскости, в которой находится точка с координатами Х и У, причем X ≠ 0 и Y ≠ 0
+// 20. Ввести номер четверти, показать диапазоны для возможных координат
+// 21. Программа проверяет пятизначное число на палиндромом.
+// 22. Найти расстояние между точками в пространстве 2D/3D
