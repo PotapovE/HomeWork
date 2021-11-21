dea3018b907f3688f97d0ce96e9a768a202002fe
PotapovE <potapovomsk@gmail.com>
2021-11-21 15:29:55 +0300

    решение задачи 41

---
+++ Test/Program.cs
@@ -0,0 +1,8 @@
+﻿// 41. Выяснить являются ли три числа сторонами треугольника
+// a < b + c; b < a + c; c < a + b - треугольник существует, если выполняются эти неравентсва.
+
+bool check (int a, int b, int c){
+    if (a < b + c & b < a + c & c < a +b) {return true;}
+    return !true;
+}
+Console.Write(check(5, 11, 5));
