206b21cb7db507a871ae8fa9d9e09320149dee8a
PotapovE <potapovomsk@gmail.com>
2021-12-03 22:12:09 +0300

    Решение задачи 66

--- Recursion/Program.cs
+++ Recursion/Program.cs
@@ -1,1 +1,11 @@
-﻿
+﻿/*//    66. Показать натуральные числа от 1 до N, N задано
+
+string ShowNaturalNumbers(int n)
+{
+    if (n <= 1) return "1";
+    else return $"{ShowNaturalNumbers(n - 1)} {n}";
+}
+
+Console.WriteLine(ShowNaturalNumbers(6));
+*/
+
