4d33140b3f73aca244b8c53bd11a3e2459dfef04
PotapovE <potapovomsk@gmail.com>
2021-12-03 22:35:33 +0300

    Решение задачи 67

--- Recursion/Program.cs
+++ Recursion/Program.cs
@@ -8,4 +8,15 @@ string ShowNaturalNumbers(int n)
 
 Console.WriteLine(ShowNaturalNumbers(6));
 */
+/*//    67. Показать натуральные числа от N до 1, N задано
+string ShowNaturalNumbers(int n)
+{
+    string vulueNumbers = string.Empty;
+    if (n == 1) return "1";
+    if (n <= 0) {vulueNumbers = $"{n} {ShowNaturalNumbers(n + 1)}";}
+    else vulueNumbers = $"{n} {ShowNaturalNumbers(n - 1)}";
+    return vulueNumbers;
+}
 
+Console.WriteLine(ShowNaturalNumbers(6));
+*/
--- Test/Program.cs
+++ Test/Program.cs
@@ -1,10 +1,1 @@
-﻿string ShowN(int n)
-{
-    if (n <= 0)
-    {
-        return "0";
-    }
-    else return $"{ShowN(n - 1)} {n}";
-}
-
-System.Console.WriteLine(ShowN(5));
+﻿
